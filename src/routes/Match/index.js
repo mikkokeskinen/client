daccb03829ba3b6c2fde66df6828aab0ae9f5e79
Mikko Keskinen <mikko.keskinen@anders.fi>
2018-06-11 09:22:51 +0300

    Revert "Add ability to pause with keyboard space"
    This reverts commit 75fa5b8d4efc1e5edfb26646c2bc74bcd6ece036.

--- src/routes/Match/index.js
+++ src/routes/Match/index.js
@@ -111,7 +111,6 @@ class Match extends React.Component {
 
     componentDidMount() {
         window.addEventListener('resize', this.updateMapSize.bind(this))
-        window.addEventListener('keydown', this.keyboardPause.bind(this))
         this.updateMapSize()
     }
 
@@ -135,15 +134,8 @@ class Match extends React.Component {
         }
     }
 
-    keyboardPause = e => {
-        if (e.keyCode === 32) {
-            this.toggleAutoplay()
-        }
-    }
-
     componentWillUnmount() {
         window.removeEventListener('resize', this.updateMapSize.bind(this))
-        window.removeEventListener('keydown', this.keyboardPause.bind(this))
     }
 
     loadTelemetry = async () => {
